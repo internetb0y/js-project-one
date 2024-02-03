23544a4d5b2e145412399098fc96eb6bef5acbbc
Aji Wicaksana <48978137+internetb0y@users.noreply.github.com>
2024-02-03 19:16:31 +0700

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,12 +1,65 @@
 // Declare And Initialize Variable
-const btn_enter=document.getElementById("enter");const btn_clear=document.getElementById("clear");const input=document.getElementById("input-box");const del_node=document.querySelector(".wrapper-ul");var li,button,div,ul;
+const btn_enter = document.getElementById("enter");
+const btn_clear = document.getElementById("clear");
+const inpt_list = document.getElementById("input-list");
+
+const btn_dlete = document.querySelector("ul");
+
+var button, li, ul, section;
 
 // Declare And Initialize Function
-function textLength(){return input.value.length;}
-function clearList(){ul=document.querySelector("ul");ul.innerHTML=" ";}
-function deleteList(event){if(event.target.tagName=="BUTTON"){li=event.target.parentNode;ul=li.parentNode;ul.removeChild(li);}}
-function addListClick(){if(textLength()>0){button=document.createElement("button");button.innerHTML=" Delete ";button.classList.add("btn-delete");li=document.createElement("li");li.appendChild(document.createTextNode(input.value));div=document.createElement("div");div.classList.add("wrapper-li");div.appendChild(li);div.appendChild(button);ul=document.querySelector("ul");ul.appendChild(div);input.value="";}}
-function addListPress(event){if(textLength()>0&&event.keyCode==13){button=document.createElement("button");button.innerHTML=" Delete ";button.classList.add("btn-delete");li=document.createElement("li");li.appendChild(document.createTextNode(input.value));div=document.createElement("div");div.classList.add("wrapper-li");div.appendChild(li);div.appendChild(button);ul=document.querySelector("ul");ul.appendChild(div);input.value="";}}
+function addListClick() {
+    if (inpt_list.value.length > 0) {
+        button = document.createElement("button");
+        button.classList.add("delete");
+        button.innerHTML = "Delete";
+
+        li = document.createElement("li");
+        li.classList.add("list");
+        li.appendChild(document.createTextNode(inpt_list.value));
+        li.appendChild(button);
+
+        ul = document.querySelector("ul");
+        ul.appendChild(li);
+
+        inpt_list.value = "";
+    }
+}
+
+function clearList() {
+    ul = document.querySelector("ul");
+    ul.innerHTML = " ";
+}
+
+function addListPress(event) {
+    if (inpt_list.value.length > 0 && event.keyCode === 13) {
+        button = document.createElement("button");
+        button.classList.add("delete");
+        button.innerHTML = "Delete";
+
+        li = document.createElement("li");
+        li.classList.add("list");
+        li.appendChild(document.createTextNode(inpt_list.value));
+        li.appendChild(button);
+
+        ul = document.querySelector("ul");
+        ul.appendChild(li);
+
+        inpt_list.value = "";
+    }
+}
+
+function deleteList(event) {
+    if (event.target.tagName === "BUTTON") {
+        li = event.target.parentNode;
+        ul = li.parentNode;
+        ul.removeChild(li);
+    }
+}
 
 // Main Function
-input.addEventListener("keypress",addListPress);btn_enter.addEventListener("click",addListClick);btn_clear.addEventListener("click",clearList);del_node.addEventListener("click",deleteList);
+btn_enter.addEventListener("click", addListClick)
+btn_clear.addEventListener("click", clearList)
+inpt_list.addEventListener("keypress", addListPress)
+
+btn_dlete.addEventListener("click", deleteList)
